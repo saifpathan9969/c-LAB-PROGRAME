556b19b8a518132a07dcbc82d8779eb4cee84710
saifpathan9969 <saifullahpathan49@gmail.com>
2024-08-20 16:22:01 +0530

    Add files via upload
    here we have compared 3 expressions 
    where the input is a= 4 and b=9
    first expression is a<b ?
    second expression is b :
    third expression is c;

---
+++ Main (6).cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a = 4,b = 6;
+  //Conditional operatorint 
+  int result = (a < b) ? b:a;
+  cout <<"The greatest number is" <<result<<endl;
+  return 0;
+}
